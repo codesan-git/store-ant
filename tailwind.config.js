bcfa03d93b4eb17ce85a45316c61c7dc0d49a003
Sabila Jamal <sabilajml@gmail.com>
2023-03-30 15:59:42 +0700

    add daisy theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["garden", "garden", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 }
