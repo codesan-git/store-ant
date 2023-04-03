68d3cbfb26e6788ce1c0d4ad02ca53a96e7b376f
Sabila Jamal <sabilajml@gmail.com>
2023-04-03 08:55:42 +0700

    null check

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -5,12 +5,6 @@ import { useSession } from 'next-auth/react'
 import { prisma } from "../../lib/prisma"
 import Link from 'next/link'
 
-interface FormData{
-    username: string,
-    phonenumber: string,
-    password: string
-}
-
 interface Props{
     shop:{
         id: Number,
@@ -43,61 +37,65 @@ export default function Profile({shop, products} : Props) {
         console.log(error)
     }
   }
-
-  return (
-    <div className='flex mx-10 my-10'>
-        <div className='w-1/6 ml-5 -mt-10'>
-            <div className='text-center justify-center mt-10'>
-                <div className="avatar">
-                    <div className="w-24 rounded-full">
-                        <img src={session?.user?.image!} width="300" height="300" />
-                    </div>
-                </div>
-                <div className='details font-bold text-lg'>
-                    <h5>{shop.shopName}</h5>
-                </div>
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-                <ul className="menu p-4 w-full bg-base-100 text-base-content">
-                <li><a>Profile</a></li>
-                <li><a>Products</a></li>
-                <li><a>Orders</a></li>
-                <li><a>Stats</a></li>
-                </ul>
-            </div>
-        </div>
-        <div className='w-full mx-10 bg-gray-100 py-5'>
-            <section className="w-4/5 mx-auto flex flex-col gap-10">
-                <div className="title">
-                <div className='flex'>
-                    <h1 className="text-gray-800 text-4xl font-bold py-4">Products</h1>
-                    <div className='ml-auto my-auto'>
-                    <Link className='px-10 py-2 rounded-sm bg-indigo-500 text-gray-50' href={'/product/create'}>Add Product</Link>
-                    </div>
-                </div>
-                <p className="mx-auto text-gray-400">products on your shop</p>
-                </div>
-            </section>
-            <div className='w-4/5 py-5 mx-auto flex-col grid lg:grid-cols-3 gap-10'>
-                {products.map(product =>(
-                    <div data-theme="garden" className="card w-auto glass" key={product.id}>
-                    <figure><img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/01/Featured-Image-Odd-Jobs-Cropped.jpg" alt="image!"/></figure>
-                    <div className="card-body py-3">
-                        <h2 className="card-title">{product.name}</h2>
-                        <p className='text-md'>Rp. {product.price}</p>                
-                        <p className='text-md'>Qty. {product.stock}</p>
-                        <div className="card-actions justify-end my-2">
-                        <button onClick={() => router.push({pathname: '/product/update', query: { id: product.id }})} className="w-16 btn btn-primary">Edit</button>
-                        <button onClick={() => deleteProduct(product.id)} className="w-16 btn bg-red-500">Delete</button>
+  
+  if(!shop){
+    router.push('/shop/register')
+  }else{
+    return (
+        <div className='flex mx-10 my-10'>
+            <div className='w-1/6 ml-5 -mt-10'>
+                <div className='text-center justify-center mt-10'>
+                    <div className="avatar">
+                        <div className="w-24 rounded-full">
+                            <img src={session?.user?.image!} width="300" height="300" />
                         </div>
                     </div>
+                    <div className='details font-bold text-lg'>
+                        <h5>{shop.shopName}</h5>
                     </div>
-                ) )}
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
+                    <ul className="menu p-4 w-full bg-base-100 text-base-content">
+                    <li><a>Profile</a></li>
+                    <li><a>Products</a></li>
+                    <li><a>Orders</a></li>
+                    <li><a>Stats</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className='w-full mx-10 bg-gray-100 py-5'>
+                <section className="w-4/5 mx-auto flex flex-col gap-10">
+                    <div className="title">
+                    <div className='flex'>
+                        <h1 className="text-gray-800 text-4xl font-bold py-4">Products</h1>
+                        <div className='ml-auto my-auto'>
+                        <Link className='px-10 py-2 rounded-sm bg-indigo-500 text-gray-50' href={'/product/create'}>Add Product</Link>
+                        </div>
+                    </div>
+                    <p className="mx-auto text-gray-400">products on your shop</p>
+                    </div>
+                </section>
+                <div className='w-4/5 py-5 mx-auto flex-col grid lg:grid-cols-3 gap-10'>
+                    {products.map(product =>(
+                        <div data-theme="garden" className="card w-auto glass" key={product.id}>
+                        <figure><img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/01/Featured-Image-Odd-Jobs-Cropped.jpg" alt="image!"/></figure>
+                        <div className="card-body py-3">
+                            <h2 className="card-title">{product.name}</h2>
+                            <p className='text-md'>Rp. {product.price}</p>                
+                            <p className='text-md'>Qty. {product.stock}</p>
+                            <div className="card-actions justify-end my-2">
+                            <button onClick={() => router.push({pathname: '/product/update', query: { id: product.id }})} className="w-16 btn btn-primary">Edit</button>
+                            <button onClick={() => deleteProduct(product.id)} className="w-16 btn bg-red-500">Delete</button>
+                            </div>
+                        </div>
+                        </div>
+                    ) )}
+                </div>
             </div>
         </div>
-    </div>
-  )
+      )
+  }
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
