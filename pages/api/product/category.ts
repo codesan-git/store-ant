d9996f231d62bca8ff2e1a1bcc8a72fead694f72
Sabila Jamal <sabilajml@gmail.com>
2023-04-11 15:00:05 +0700

    migration & category api

---
+++ pages/api/product/category.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import { prisma } from "../../../lib/prisma"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const categories = await prisma.category.findMany({
+      select:{
+          id: true,
+          category: true
+      }
+    })
+  res.status(200).json({categories})
+  console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
