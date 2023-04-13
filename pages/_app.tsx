9dbb8c2d8c9a0133639fc290e9bfab76bd1a3573
boblightning <satrianugraha4000@gmail.com>
2023-04-13 09:08:55 +0700

    sorting FE & BE with ERROR TYPING

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,11 @@ import { Session } from "next-auth"
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 import "../styles/globals.css"
+// import 'primereact/resources/themes/saga-blue/theme.css';
+import "primereact/resources/themes/soho-light/theme.css";
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+// import 'primeflex/primeflex.css';
 
 const App = ({ 
   Component, pageProps 
