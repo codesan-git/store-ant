0dea19bc25b60641e0ac2525944b6e9a5d2bb231
boblightning <satrianugraha4000@gmail.com>
2023-05-02 10:44:05 +0700

    FE: validation page

--- pages/validation.tsx
+++ pages/validation.tsx
@@ -6,11 +6,13 @@ import { getSession, useSession } from "next-auth/react";
 export default function Validation({ token }: any) {
   const router = useRouter();
   const { token: accessToken } = router.query;
-  const { data: session } = useSession(); 
+  const { data: session } = useSession();
 
   const verifyAccount = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/api/profile/verified");
+      const response = await axios.get(
+        "http://localhost:3000/api/profile/verified"
+      );
       console.log("dari fetchProduct", response.data);
     } catch (error) {
       console.log(error);
@@ -19,24 +21,36 @@ export default function Validation({ token }: any) {
 
   return (
     <>
-    {accessToken == session?.user.accessToken! ? (
-      <>
-        <button
-          className="btn btn-outline btn-primary w-full btn-md rounded-lg"
-          onClick={verifyAccount}
-        >
-          Verify Account
-        </button>
-      </> 
-    )
-    :
-    (
-      <>
-      <p>404</p>
-      </>
-    )
-  }
-  </>
+      {accessToken == session?.user.accessToken! ? (
+        <>
+          <div className="card lg:w-96 bg-base-100 shadow-xl mx-auto lg:my-36">
+            <figure className="px-10 pt-10">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/shipsy-public-assets/shipsy/SHIPSY_LOGO_BIRD_BLUE.png"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Verify Your Account Here!</h2>
+              <p>Lets dance together, dance on the dancefloor</p>
+              <div className="card-actions">
+                <button
+                  className="btn btn-primary w-full rounded-md"
+                  onClick={verifyAccount}
+                >
+                  Verify Account
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>404</p>
+        </>
+      )}
+    </>
   );
 }
 
