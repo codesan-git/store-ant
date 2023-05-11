3a20fd1b70584634cbf5969defc4d1e37de1f643
peterhadymmdn <peter.hady@mmdn.co.id>
2023-05-11 16:39:30 +0700

    Added base mobile styling for Product Form Page

--- pages/product/create.tsx
+++ pages/product/create.tsx
@@ -62,7 +62,24 @@ export default function CreateProduct() {
       <h1 className='text-2xl text-gray-800 font-bold'>Add Product</h1>
       <form action="">
         <section>
-          {/* <input type="file" name="" id="" /> */}
+          <div className='border-gray-600 border border-dashed flex justify-center items-center h-40 relative'>
+            <input type="file" accept='.jpg, .jpeg .png, .svg .webp' name="product-image" id="product-image-input" className='w-full h-full cursor-pointer opacity-0 absolute' 
+              onChange={({target}) => {
+                if(target.files){
+                    const file = target.files[0];
+                    setSelectedImage(URL.createObjectURL(file));
+                    setSelectedFile(file);
+                }
+            }}
+            />
+            <label htmlFor="product-image-input" className='hover:cursor-pointer flex flex-row'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+              </svg>
+              &nbsp;Select Image
+            </label>
+          </div>
         </section>
         <section  className='space-y-4 p-2 flex flex-col'>
           <div className='flex flex-col space-y-1'>
