03ecf8bfba4f4a20e399dac36df5a4427b7eb9f5
peterhadymmdn <peter.hady@mmdn.co.id>
2023-05-12 11:55:58 +0700

    Added styling for product form web page

--- pages/product/create.tsx
+++ pages/product/create.tsx
@@ -59,10 +59,10 @@ export default function CreateProduct() {
 
   return (
     <div>
-      <h1 className='text-2xl text-gray-800 font-bold'>Add Product</h1>
-      <form action="">
-        <section>
-          <div className='border-gray-600 border border-dashed flex justify-center items-center h-40 relative'>
+      <h1 className='text-2xl text-gray-800 font-bold mb-2'>Add Product</h1>
+      <form action="" className='lg:flex lg:flex-row lg:justify-center lg:px-10'>
+        <section className='lg:w-1/3 lg:flex lg:justify-center lg:items-center'>
+          <div className='border-gray-600 border border-dashed rounded-xl flex justify-center items-center h-40 lg:h-5/6 lg:w-5/6 relative'>
             <input type="file" accept='.jpg, .jpeg .png, .svg .webp' name="product-image" id="product-image-input" className='w-full h-full cursor-pointer opacity-0 absolute' 
               onChange={({target}) => {
                 if(target.files){
@@ -74,41 +74,42 @@ export default function CreateProduct() {
             />
             <label htmlFor="product-image-input" className='hover:cursor-pointer flex flex-row'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
-                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
               </svg>
               &nbsp;Select Image
             </label>
           </div>
         </section>
-        <section  className='space-y-4 p-2 flex flex-col'>
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor="product-name-input">Name</label>
-            <input  id='product-name-input' name='product-name' type="text" className='border border-black focus:border-none' />
+        <section  className='p-4 lg:w-1/2'>
+          <div className=' space-y-4 flex flex-col justify-center items-center'>
+            <div className='flex flex-col space-y-1 w-full'>
+              <label htmlFor="product-name-input" className='font-bold'>Name</label>
+              <input  id='product-name-input' name='product-name' type="text" className='p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white'/>
+            </div>
+            <div className='flex flex-col space-y-1 w-full'>
+              <label htmlFor="product-category-input" className='font-bold'>Category</label>
+              <select name="product-category" id="product-category-input" className='p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white'>
+                <option value="">Makanan</option>
+                <option value="">Waifu</option>
+                <option value="">Pasokon</option>
+              </select>
+            </div>
+            <div className='flex flex-col space-y-1 w-full'>
+              <label htmlFor="product-quantity-input" className='font-bold'>Quantity</label>
+              <input id='product-quantity-input' name='product-quantity' type="number" className='p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white'/>
+            </div>
+            <div className='flex flex-col space-y-1 w-full'>
+              <label htmlFor="product-price-input" className='font-bold'>Price</label>
+              <input id='product-price-input' name='product-price' type="number" className='p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white'/>
+            </div>
+            <div className='flex flex-col space-y-1 w-full'>
+              <label htmlFor="" className='font-bold'>Description</label>
+              <textarea name="product-description" id="product-description-input" className='p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white'/>
+            </div>
+            <button className='btn fill-indigo-700 lg:w-28'>
+              Submit
+            </button>
           </div>
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor="">Category</label>
-            <select name="product-category" id="product-category-input" className='h-8 p-2'>
-              <option value="">Makanan</option>
-              <option value="">Waifu</option>
-              <option value="">Pasokon</option>
-            </select>
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor="product-quantity-input">Quantity</label>
-            <input id='product-quantity-input' name='product-quantity' type="number" className='border border-black focus:border-none'/>
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor="product-price-input">Price</label>
-            <input id='product-price-input' name='product-price' type="number" className='border border-black focus:border-none'/>
-          </div>
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor="">Description</label>
-            <textarea name="product-description" id="product-description-input" className='h-36 border border-black focus:border-none'/>
-          </div>
-          <button className='btn fill-indigo-700'>
-            Submit
-          </button>
         </section>
       </form>
     </div>
