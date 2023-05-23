902b0d3757b4585c1710b60d37600391b5fab2f3
peterhadymmdn <peter.hady@mmdn.co.id>
2023-05-23 15:12:51 +0700

    Finalize styling

--- pages/product/create.tsx
+++ pages/product/create.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import useSWR from 'swr';
 import { Category } from '@prisma/client';
 import axios from 'axios';
+import { HiOutlineCamera, HiOutlinePhoto } from 'react-icons/hi2';
 
 const fetchProducts = async(url: string) => {
     const response = await fetch(url);
@@ -30,7 +31,7 @@ export default function CreateProduct() {
   const [form, setForm] = useState<FormData>({name: '', price: '', stock:'', description: '', categoryId: '1'});
   const router = useRouter()
   const [selectedImage, setSelectedImage] = useState<string>();
-  const [files, setFile] = useState<any[]>([]);
+  const [selectedFiles, setFile] = useState<any[]>([]);
 
   const {data, isLoading} = useSWR<{categories : Array<Category>}>(
     `/api/category/`,
@@ -51,25 +52,25 @@ export default function CreateProduct() {
       const fileType = file[i]["type"];
       const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
       if (validImageTypes.includes(fileType)) {
-        setFile([...files, file[i]]);
+        setFile([...selectedFiles, file[i]]);
       } else {
         console.log("only images accepted");
       }
-      console.log("FILES: ", files);
+      console.log("FILES: ", selectedFiles);
     }
   };
   
   const removeImage = (i: string) => {
-    setFile(files.filter((x) => x.name !== i));
-    if(files.length >= 2)
-      setSelectedImage(URL.createObjectURL(files[files.length - 2]));
+    setFile(selectedFiles.filter((x) => x.name !== i));
+    if(selectedFiles.length >= 2)
+      setSelectedImage(URL.createObjectURL(selectedFiles[selectedFiles.length - 2]));
   };
   
   const handleUpload = async () => {
     try {
-        if(files.length == 0) return;
+        if(selectedFiles.length == 0) return;
         const formData = new FormData();
-        files.forEach((file) => formData.append("image", file) );
+        selectedFiles.forEach((file) => formData.append("image", file) );
         //formData.append("image", selectedFile);
         formData.append("name", form.name);
         formData.append("price", form.price);
@@ -84,27 +85,46 @@ export default function CreateProduct() {
 
   const renderInputMessage = () => {
 
-    if(files.length >= 5) return (
+    if(selectedFiles.length >= 5) return (
       <>
-        <label htmlFor="product-image-input" className='hover:cursor-pointer flex flex-row'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-          </svg>
-          &nbsp;You have reached the max limit for photos.
+        <label htmlFor="product-image-input" className='hover:cursor-pointer flex flex-col lg:flex-row justify-center items-center'>
+          <HiOutlinePhoto className='w-6 h-6'/>
+          <p className='text-xs lg:text-base'>&nbsp;You have reached the max limit for photos.</p>
         </label>          
       </>
     );
     
     return (
       <>
-        <label htmlFor="product-image-input" className='hover:cursor-pointer flex flex-row'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-          </svg>
+        <label htmlFor="product-image-input" className='hover:cursor-pointer flex flex-col lg:flex-row justify-center items-center'>
+          <HiOutlineCamera className='w-6 h-6'/>
           &nbsp;Select Image
         </label>
       </>
     );
+  }
+
+  const renderSelectedImages = () => {
+    if(selectedFiles.length == 0) return;
+
+    return (
+      <>
+        <div className='flex flex-row gap-2'>
+          {
+            selectedFiles.map( 
+              (file, key) => 
+                <div key={key} className="relative">
+                  <div onClick={() => removeImage(file.name)} className="flex justify-center items-center bg-black text-white rounded-full h-4 w-4 text-xs font-bold absolute -right-2 -top-2 sm:-right-2 hover:cursor-pointer">
+                    ✕
+                  </div>
+                  <img src={URL.createObjectURL(file)} alt="" className="w-12 h-12 sm:w-16 sm:h-16 object-cover border border-gray-600" />
+                </div>
+            )
+          }
+        </div>
+      </>
+    );
+
   }
 
   return (
@@ -117,9 +137,9 @@ export default function CreateProduct() {
         </section>
       </div>
       <form action="" onSubmit={e=>{e.preventDefault(); handleUpload();}} className='lg:flex lg:flex-row'>
-        <section className='px-4 lg:w-1/2 flex lg:flex-col justify-center items-center'>
+        <section className='px-4 lg:w-1/2 flex flex-col justify-center items-center space-y-4'>
           <div className='border-gray-600 border border-dashed rounded-xl flex justify-center items-center h-40 w-full lg:h-5/6 lg:w-5/6 relative'>
-            <input disabled={files.length >= 5} type="file" accept='.jpg, .jpeg, .png, .webp' name="product-image" id="product-image-input" className='w-full h-full cursor-pointer opacity-0 absolute' 
+            <input disabled={selectedFiles.length >= 5} type="file" accept='.jpg, .jpeg, .png, .webp' name="product-image" id="product-image-input" className='w-full h-full cursor-pointer opacity-0 absolute' 
               onChange={({target}) => {
                 handleFile(target); 
                 if(target.files){
@@ -131,24 +151,7 @@ export default function CreateProduct() {
             />
             {renderInputMessage()}
           </div>
-          <div className='flex flex-wrap gap-2 mt-2'> {/**/}
-            {files.map((file, key) => {
-                return (
-                  <div key={key} className="relative overflow-hidden">
-                    <div
-                      onClick={() => {
-                        removeImage(file.name);
-                      }}
-                      className="right-1 hover:text-white cursor-pointer bg-red-400"
-                    >Remove</div>
-                    <img
-                      className="h-20 w-20 rounded-md"
-                      src={URL.createObjectURL(file)}
-                    />
-                  </div>
-                );
-              })}
-          </div>
+          {renderSelectedImages()}
         </section>
         <section  className='p-4 lg:w-1/2'>
           <div className=' space-y-4 flex flex-col'>
