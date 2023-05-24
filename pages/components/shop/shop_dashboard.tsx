7273ab745d9c2d65370fa4fe7b83aea1d157f198
peterhadymmdn <peter.hady@mmdn.co.id>
2023-05-24 15:46:51 +0700

    Adjusted routing for shop

--- pages/components/shop/shop_dashboard.tsx
+++ pages/components/shop/shop_dashboard.tsx
@@ -23,76 +23,74 @@ const ShopDashboard = ({ shop }: Props) => {
   const [isSalesDropdownClosed, setIsSalesDropdownClosed] = useState<Boolean>(true);
 
   return(
-    <>
-      <div id='dashboard' className="lg:shadow-md lg:w-1/6"> {/*Try to use drawer here*/}
-        <div id="shop-profile" className="flex flex-col justify-center items-center px-2 py-4 space-y-2 border border-b-black">
-          <div id="profile-photo-container" className="avatar">
-            <div className="w-24 rounded-full border border-black">
-              <img src={session?.user.image!} alt="" />
-            </div>
+    <div id='dashboard' className="lg:shadow-md lg:w-1/6"> {/*Try to use drawer here*/}
+      <div id="shop-profile" className="flex flex-col justify-center items-center px-2 py-4 space-y-2">
+        <div id="profile-photo-container" className="avatar">
+          <div className="w-24 rounded-full border border-black">
+            <img src={session?.user.image!} alt="" />
           </div>
-          <h1 className="font-bold">{shop.shopName}</h1>
-          <h1 className="font-bold">Rating: {shop.averageRating.toString()}/5</h1>
-          <button className="rounded-md bg-green-500 hover:bg-green-400 transition duration-200 p-1 w-24 text-white">Edit</button>
         </div>
-        <div id="shop-stats" className="p-2 border border-b-black">
-          <h1>Balance: Rp.30.000,00</h1>
-        </div>
-        <div id="dashboard-navigation" className="p-2">
-          <ul className="">
-            <li>
-              <Link href={''} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                <HiHome className="h-6 w-6"/>
-                <span className="ml-3">Home</span>
-              </Link>
-            </li>
-            <li>
-              <Link href={''} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                <HiChatBubbleBottomCenterText className="h-6 w-6"/>
-                <span className="ml-3">Chat</span>
-              </Link>
-            </li>
-            <li>
-              <Link href={''} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                <HiShoppingBag className="h-6 w-6"/>
-                <span className="ml-3">Products</span>
-              </Link>
-            </li>
-            <li>
-              <button type="button" onClick={() => setIsSalesDropdownClosed(!isSalesDropdownClosed)} className="flex items-center whitespace-nowrap w-full p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300" >
-                <HiCurrencyDollar className="h-6 w-6"/>
-                <span className="flex-1 ml-3 text-left">Sales</span>
-                {isSalesDropdownClosed ? <HiChevronDown className="h-6 w-6"/> : <HiChevronUp className="h-6 w-6"/>}
-              </button>
-              <ul id="sales-ul-dropdown" hidden={isSalesDropdownClosed.valueOf()} className="transition duration-75 bg-gray-200">
-                <li>
-                  <Link href={''} className="flex p-2 pl-11 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                    Orders
-                  </Link>
-                </li>
-                <li>
-                  <Link href={''} className="flex p-2 pl-11 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                    Complaints
-                  </Link>
-                </li>
-                <li>
-                  <Link href={''} className="flex p-2 pl-11 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                    Reviews
-                  </Link>
-                </li>
-              </ul>
-            </li>
-              {/*Create dropdown menu here using https://tailwindcomponents.com/component/tailwind-css-sidebar-dropdown*/}
-            <li>
-              <Link href={''} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
-                <HiChartPie className="h-6 w-6"/>
-                <span className="ml-3">Stats</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <h1 className="font-bold">{shop.shopName}</h1>
+        <h1 className="font-bold">Rating: {shop.averageRating.toString()}/5</h1>
+        <button className="rounded-md bg-green-500 hover:bg-green-400 transition duration-200 p-1 w-24 text-white">Edit</button>
       </div>
-    </>
+      <div id="shop-stats" className="py-2 px-4 border border-y-gray-600">
+        <h1>Balance: Rp.30.000,00</h1>
+      </div>
+      <div id="dashboard-navigation" className="p-2">
+        <ul className="">
+          <li>
+            <Link href={'/shop'} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+              <HiHome className="h-6 w-6"/>
+              <span className="ml-3">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={''} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+              <HiChatBubbleBottomCenterText className="h-6 w-6"/>
+              <span className="ml-3">Chat</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/shop/products'} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+              <HiShoppingBag className="h-6 w-6"/>
+              <span className="ml-3">Products</span>
+            </Link>
+          </li>
+          <li>
+            <button type="button" onClick={() => setIsSalesDropdownClosed(!isSalesDropdownClosed)} className="flex items-center whitespace-nowrap w-full p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300" >
+              <HiCurrencyDollar className="h-6 w-6"/>
+              <span className="flex-1 ml-3 text-left">Sales</span>
+              {isSalesDropdownClosed ? <HiChevronDown className="h-6 w-6"/> : <HiChevronUp className="h-6 w-6"/>}
+            </button>
+            <ul id="sales-ul-dropdown" hidden={isSalesDropdownClosed.valueOf()} className="transition duration-75 bg-gray-200">
+              <li>
+                <Link href={'/shop/orders'} className="flex p-2 pl-11 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+                  Orders
+                </Link>
+              </li>
+              <li>
+                <Link href={''} className="flex p-2 pl-11 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+                  Complaints
+                </Link>
+              </li>
+              <li>
+                <Link href={''} className="flex p-2 pl-11 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+                  Reviews
+                </Link>
+              </li>
+            </ul>
+          </li>
+            {/*Create dropdown menu here using https://tailwindcomponents.com/component/tailwind-css-sidebar-dropdown*/}
+          <li>
+            <Link href={''} className="flex p-2 text-base font-normal rounded-lg transition duration-200 hover:bg-gray-300">
+              <HiChartPie className="h-6 w-6"/>
+              <span className="ml-3">Stats</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 
 }
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -32,26 +32,9 @@ interface Category{
 }
 
 export default function Profile({shop, products} : Props) {
+
   const router = useRouter()
-  const{data:session} = useSession()
-
-  async function deleteProduct(id:string) {
-    try{
-      fetch(`http://localhost:3000/api/product/${id}`, {
-        body: JSON.stringify(id),
-          headers: {
-              'Content-Type' : 'application/json'
-          },
-        method: 'DELETE'
-      }).then(()=>{
-        router.replace(router.asPath)
-      })
-    }catch(error){
-        //console.log(error)
-    }
-  }
-
-  const editProduct = (id: string) => router.push({pathname: '/product/update', query: { id: id }});
+  const{data:session} = useSession();
 
   
   if(!shop){
@@ -60,48 +43,11 @@ export default function Profile({shop, products} : Props) {
     return (
       <>
       <Navbar />
-        <div className='flex mx-10 my-10'>
+        <div className='flex mx-10 my-10 space-x-4'>
             <ShopDashboard shop={shop}/>
-            <div id='shop-dashboard' className='w-1/6 ml-5 -mt-10'>
-                <div className='text-center justify-center mt-10'>
-                    <div className="avatar">
-                        <div className="w-24 rounded-full">
-                            <img src={session?.user?.image!} width="300" height="300" />
-                        </div>
-                    </div>
-                    <div className='details font-bold text-lg'>
-                        <h5>{shop.shopName}</h5>
-                        <p>Rating: {String(shop.averageRating)}/ 5</p>
-                    </div>
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-                    <ul className="menu p-4 w-full bg-base-100 text-base-content">
-                    <li><Link href="/shop">Profile</Link></li>
-                    <li><Link href="">Products</Link></li>
-                    <li><Link href="/shop/orders">Orders</Link></li>
-                    <li><Link href="/shop/stats">Stats</Link></li>
-                    </ul>
-                </div>
+            <div id='product-list' className='w-full bg-gray-100 py-5'>
+                
             </div>
-            {/* <div id='product-list' className='w-full bg-gray-100 py-5'>
-                <section className="w-4/5 mx-auto flex flex-col gap-10">
-                    <div className="title">
-                    <div className='flex'>
-                        <h1 className="text-gray-800 text-4xl font-bold py-4">Products</h1>
-                        <div className='ml-auto my-auto'>
-                        <Link className='px-10 py-2 rounded-sm bg-indigo-500 text-gray-50' href={'/product/create'}>Add Product</Link>
-                        </div>
-                    </div>
-                    <p className="mx-auto text-gray-400">products on your shop</p>
-                    </div>
-                </section>
-                <div className='w-4/5 py-5 mx-auto flex-col grid lg:grid-cols-3 gap-10'>
-                    {products.map(product =>(
-                          <ProductCard product={product} onEdit={editProduct} onDelete={deleteProduct} key={product.id}/>                        
-                    ) )}
-                </div>
-            </div> */}
         </div>
         <Footer />
         </>
