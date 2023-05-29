7a45ae2cc3015059891b84817725bde802dc29ae
peterhadymmdn <peter.hady@mmdn.co.id>
2023-05-29 11:09:20 +0700

    Update prisma

--- pages/transactions.tsx
+++ pages/transactions.tsx
@@ -4,27 +4,10 @@ import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { getSession, useSession } from "next-auth/react";
 import { prisma } from "../lib/prisma";
 import Link from "next/link";
-import ProductCard from "../components/product_card";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import { Status } from "@prisma/client";
-import type { TabsStylesType } from "@material-tailwind/react";
-
-// TABS
-import {
-  Tabs,
-  TabsHeader,
-  TabsBody,
-  Tab,
-  TabPanel,
-} from "@material-tailwind/react";
-import {
-  Square3Stack3DIcon,
-  UserCircleIcon,
-  Cog6ToothIcon,
-} from "@heroicons/react/24/solid";
 import ReviewModal from "../components/transactions/review-modal";
-// END TABS
 
 interface CartItems {
   cartItems: {
@@ -62,34 +45,6 @@ export default function Transaction({ cartItems }: CartItems) {
   const [open, setOpen] = React.useState(false);
   const [currentRateProductName, setCurrentRateProductName] = useState<String>("");
   const [currentCartItemId, setCurrentCartItemId] = useState<Number>();
-
-  //  TABS
-  const data = [
-    {
-      label: "Dashboard",
-      value: "dashboard",
-      icon: Square3Stack3DIcon,
-      desc: `It really matters and then like it really doesn't matter.
-      What matters is the people who are sparked by it. And the people 
-      who are like offended by it, it doesn't matter.`,
-    },
-    {
-      label: "Profile",
-      value: "profile",
-      icon: UserCircleIcon,
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-    {
-      label: "Settings",
-      value: "settings",
-      icon: Cog6ToothIcon,
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
-  ];
-  //  END TABS
 
   if (cartItems) {
     let i: number;
