10e6f9bb1254d1bd3691c6ef63c32910568540bb
Sabila Jamal <sabilajml@gmail.com>
2023-07-04 09:56:34 +0700

    add set address button

---
+++ pages/api/address/setmain.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import { prisma } from "../../../lib/prisma"
+
+type Data = {
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const {id } = req.body
+  const session = await getSession({req})
+
+  try {
+    await prisma.address.update({
+      where:{ id: id!},
+      data: {
+        isMainAddress: true
+      }
+    })
+    res.status(200).json({ message: 'main address set' })
+  } catch (error) {
+    //console.log(error)
+    res.status(400).json({ message: "Fail" })
+  }
+}
