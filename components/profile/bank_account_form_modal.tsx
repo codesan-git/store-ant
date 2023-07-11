aaacd07cfb696c24ae5183523708bf06ffc812f0
peterhadymmdn <peter.hady@mmdn.co.id>
2023-07-11 15:39:38 +0700

    Added logic to block non-numerical input in account field

--- components/profile/bank_account_form_modal.tsx
+++ components/profile/bank_account_form_modal.tsx
@@ -1,7 +1,7 @@
 import { Button, Dialog, DialogBody, DialogHeader } from "@material-tailwind/react";
 import { BankType } from "@prisma/client";
 import { useRouter } from "next/router";
-import { Fragment, useState } from "react";
+import { ChangeEvent, Fragment, useState } from "react";
 
 interface Props {
   banks: BankType[]
@@ -16,6 +16,7 @@ interface BankAccountForm {
 const BankAccountFormModal = ({ banks } : Props) => {
 
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [accountNumber, setAccountNumber] = useState<number>()
 
   const modalOpenHandler = () => setModalOpen(!modalOpen);
 
@@ -41,6 +42,13 @@ const BankAccountFormModal = ({ banks } : Props) => {
     catch (e) {
       console.log(e);
     }
+  }
+
+  const handleInputAccountNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const result = Number(event.target.value.replace(/\D/g, ''));
+    setAccountNumber(result);
+
+    setForm({...form, number: result.valueOf().toString()})
   }
 
   return (
@@ -70,12 +78,12 @@ const BankAccountFormModal = ({ banks } : Props) => {
               </select>
             </div>
             <div id="account-owner-name-input-container" className="flex flex-col w-full space-y-1">
-              <label htmlFor="">Name</label>
-              <input onChange={e => {setForm({...form, name: e.target.value})}} type="text" className="p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white"/>
+              <label htmlFor="name-input">Name</label>
+              <input id="name-input" onChange={e => {setForm({...form, name: e.target.value})}} type="text" className="p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white"/>
             </div>
             <div id="account-number-input-container" className="flex flex-col w-full space-y-1">
-              <label htmlFor="">Account Number</label>
-              <input onChange={e => {setForm({...form, number: e.target.value})}} type="text" className="p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white"/>
+              <label htmlFor="account-number-input">Account Number</label>
+              <input id="account-number-input" value={accountNumber} onChange={handleInputAccountNumberChange} type="text" className="p-2 h-10 border rounded-lg border-gray-400 focus:border-none focus:border-white"/>
             </div>
             <div className="w-full flex justify-end">
               <button type="submit" onClick={modalOpenHandler} className="w-24 h-8 bg-green-700 hover:bg-green-500 transition text-white rounded-md ">Save</button>
