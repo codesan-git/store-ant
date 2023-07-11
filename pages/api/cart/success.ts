75d11fd0b5df08af6540ab5960cc2f557cafdf14
Sabila Jamal <sabilajml@gmail.com>
2023-07-11 16:09:50 +0700

    edit chat properly

--- pages/api/cart/success.ts
+++ pages/api/cart/success.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import { prisma } from "../../../lib/prisma"
-import { TransactionStatus } from '@prisma/client'
+import { NotifRole, NotifType, TransactionStatus } from '@prisma/client'
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,6 +17,28 @@ export default async function handler(
             status: TransactionStatus.AWAITING_CONFIRMATION,
             paymentMethod: String(paymentType)
         }
+    })
+
+    const shop = await prisma.shop.findFirst({
+      where: {id: transaction.shopId}
+    })
+
+    const notificationUser = await prisma.notification.create({
+      data:{
+        userId: session?.user.id!,
+        notifRole: NotifRole.USER,
+        notifType: NotifType.TRANSACTION,
+        body: `Pembayaran untuk transaksi ${transaction.id} telah berhasil.`
+      }
+    })
+
+    const notificationSeller = await prisma.notification.create({
+      data:{
+        userId: shop?.userId!,
+        notifRole: NotifRole.SELLER,
+        notifType: NotifType.TRANSACTION,
+        body: `Transaksi baru dengan kode ${transaction.id} telah dibuat.`
+      }
     })
     res.status(200).json({ message: "Success!" })
   } catch (error) {
--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -7,23 +7,36 @@ export default function SocketHandler(
     req: NextApiRequest,
     res: any
 ){
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
+    if(!res.socket.server.io){
+        const io = new Server(res.socket.server);
+        res.socket.server.io = io;
 
-    let users: any[] = [];
+        let users: any[] = [];
 
-    io.on("connection", (socket) =>{
-        socket.on("connect-user", (userId) => {
-            users[userId] = socket.id;
-            console.log("users: ", users[userId]);
+        io.on("connection", (socket) =>{
+            socket.on("connect-user", (userId) => {
+                console.log(userId);
+                if(userId || userId != null)
+                    users[userId] = socket.id;
+                console.log("users: ", users[userId]);
+            });
+
+            socket.on("send-message", (obj) => {
+                io.to(users[obj.recipientId]).emit("receive-message", obj);
+                console.log("users: ", users);
+                console.log("id: ", socket.id);
+            });
         });
-
-        socket.on("send-message", (obj) => {
-            io.to(users[obj.recipientId]).emit("receive-message", obj);
-            console.log("users: ", users);
-            console.log("id: ", socket.id);
-        });
-    });
-    console.log("setting socket");
+        console.log("setting socket");
+    } else {
+        console.log("server is already set");
+    }
     res.end();
 }
+
+export const config = {
+    api: {
+      bodyParser: false
+    }
+  }
+  
