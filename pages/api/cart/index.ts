9a4d96acd8fcdcb8024c150ba2fbc279a2848447
Satria Agung Nugraha <satrianugrahacode@gmail.com>
2023-08-03 13:49:11 +0700

    deploy

--- pages/api/cart/index.ts
+++ pages/api/cart/index.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import { prisma } from "../../../lib/prisma"
-import { Status } from '@prisma/client'
 
 export default async function handler(
   req: NextApiRequest,
